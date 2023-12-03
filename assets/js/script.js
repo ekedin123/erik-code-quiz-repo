fd005efb686d2424876ecddec07ac9782ca0591e
Erik Edin <ekedin@gmail.com>
2023-12-03 12:50:28 -0700

    adding more starter code and psuedo code

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+
+var totalQuestions = [
+    {
+        question: "What is the capital of France?",
+        choices: ["Paris", "Berlin", "London", "Rome"],
+        correctAnswer: 0
+    }, {
+        question: "What is the capital of France?",
+        choices: ["Paris", "Berlin", "London", "Rome"],
+        correctAnswer: 0
+    }, {
+        question: "What is the capital of France?",
+        choices: ["Paris", "Berlin", "London", "Rome"],
+        correctAnswer: 0
+    }, {
+        question: "What is the capital of France?",
+        choices: ["Paris", "Berlin", "London", "Rome"],
+        correctAnswer: 0
+    }, {
+        question: "What is the capital of France?",
+        choices: ["Paris", "Berlin", "London", "Rome"],
+        correctAnswer: 0
+    },
+  ];
+  
+
+
+
+
